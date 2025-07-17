084f5f132c0e68695fb8d5a84224e12d9bf0cd10
ROHIT P T <132969834+rohioffl@users.noreply.github.com>
2025-07-17 17:34:20 +0530

    Connect to MongoDB on startup

--- cloudscan/apps.py
+++ cloudscan/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+import os
+from mongoengine import connect
+from mongoengine.connection import get_connection
 
 
 class CloudscanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cloudscan'
+
+    def ready(self):
+        """Connect to MongoDB if ``MONGODB_URI`` is configured."""
+        mongo_uri = os.getenv("MONGODB_URI")
+        if not mongo_uri:
+            return
+        try:
+            # only connect if no default connection is registered
+            get_connection()
+        except Exception:
+            connect(host=mongo_uri)
