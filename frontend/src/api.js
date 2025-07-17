12021b175e114890ccab86bee7deb79528f6a81b
Rohit <rohitpt2003@gmail.com>
2025-07-17 19:23:15 +0530

    initialize

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://34.202.36.87:8000';
 
 const api = axios.create({
   baseURL: BASE_URL,
